45549d150aaf23dddf5b6c3437214dc23d97b4a2
Willem Nieuwenhuis <w.nieuwenhuis@utwente.nl>
2021-11-01 17:16:01 +0100

    Very simple test functions

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -1,12 +1,23 @@
 import unittest
-from gemguide.gemtodocx import convert
+import os
+from gemguide.gemtodocx import convert2docx, convert2pdf
 
 class TestConvert(unittest.TestCase):
 
     def test_xlsx2docx(self):
+        ''' Not an actual test, but easy way to perform convert
+        '''
         fn = 'GEM - LUND - Track 3  - Year 1 - Geographical Information Systems, Basic Course.xlsx'
         doc = 'output.docx'
 
-        convert(fn, doc)
+        convert2docx(fn, doc)
         self.assertTrue(True)
 
+    def test_xlsx2pdf(self):
+        ''' Not an actual test, but easy way to perform convert
+        '''
+        fn = 'GEM - LUND - Track 3  - Year 1 - Geographical Information Systems, Basic Course.xlsx'
+        pdf = 'E:/Data/studyguide/output.pdf'
+
+        convert2pdf(fn, pdf)
+        self.assertTrue(True)
